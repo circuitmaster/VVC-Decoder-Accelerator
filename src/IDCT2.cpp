7ea643bfa34acd6507395815198aa5b530f8e620
circuitmaster <aliemre.oztas@postgrad.manchester.ac.uk>
2025-12-13 00:21:55 +0000

    port widened

--- src/IDCT2.cpp
+++ src/IDCT2.cpp
@@ -312,17 +312,17 @@ void IDCT2B64(ap_int<32> in[64], ap_int<32> out[64]){
     out[63] = evens[0] - odds[0];
 }
 
-extern "C" void IDCT2(ap_int<512>* in, ap_int<512>* out, int block_size, int size, int shift, int outputMinimum, int outputMaximum){
+extern "C" void IDCT2(ap_int<1024>* in, ap_int<1024>* in2, ap_int<1024>* out, ap_int<1024>* out2, int block_size, int size, int shift, int outputMinimum, int outputMaximum){
     #pragma HLS INTERFACE m_axi port=in offset=slave bundle=gmem0
-    #pragma HLS INTERFACE m_axi port=out offset=slave bundle=gmem1
-    #pragma HLS INTERFACE s_axilite port=in bundle=control
-    #pragma HLS INTERFACE s_axilite port=out bundle=control
-    #pragma HLS INTERFACE s_axilite port=block_size bundle=control
-    #pragma HLS INTERFACE s_axilite port=size bundle=control
-    #pragma HLS INTERFACE s_axilite port=shift bundle=control
-    #pragma HLS INTERFACE s_axilite port=outputMinimum bundle=control
-    #pragma HLS INTERFACE s_axilite port=outputMaximum bundle=control
-    #pragma HLS INTERFACE s_axilite port=return bundle=control
+    #pragma HLS INTERFACE m_axi port=in2 offset=slave bundle=gmem1
+    #pragma HLS INTERFACE m_axi port=out offset=slave bundle=gmem0
+    #pragma HLS INTERFACE m_axi port=out2 offset=slave bundle=gmem1
+    #pragma HLS INTERFACE s_axilite port=block_size
+    #pragma HLS INTERFACE s_axilite port=size
+    #pragma HLS INTERFACE s_axilite port=shift
+    #pragma HLS INTERFACE s_axilite port=outputMinimum
+    #pragma HLS INTERFACE s_axilite port=outputMaximum
+    #pragma HLS INTERFACE s_axilite port=return
 
     int add = 1 << (shift - 1);
 
@@ -330,8 +330,10 @@ extern "C" void IDCT2(ap_int<512>* in, ap_int<512>* out, int block_size, int siz
         #pragma HLS LOOP_TRIPCOUNT min=1 max=1024
         #pragma HLS PIPELINE II=1
 
-        ap_int<512> in_block = in[i];
-        ap_int<512> out_block = 0;
+        ap_int<1024> in_block = in[i];
+        ap_int<1024> in_block2 = in2[i];
+        ap_int<1024> out_block = 0;
+        ap_int<1024> out_block2 = 0;
 
         if(block_size == 64){
             ap_int<32> in_data[64];
@@ -339,16 +341,26 @@ extern "C" void IDCT2(ap_int<512>* in, ap_int<512>* out, int block_size, int siz
             #pragma HLS ARRAY_PARTITION variable=in_data complete dim=0
             #pragma HLS ARRAY_PARTITION variable=out_data complete dim=0
 
-            for(int j=0; j<64; j++){
+            for(int j=0; j<32; j++){
                 #pragma HLS UNROLL
                 in_data[j] = in_block.range((j+1)*32-1, j*32);
             }
 
+            for(int j=0; j<32; j++){
+                #pragma HLS UNROLL
+                in_data[j+32] = in_block2.range((j+1)*32-1, j*32);
+            }
+
             IDCT2B64(in_data, out_data);
 
-            for(int j=0; j<64; j++){
+            for(int j=0; j<32; j++){
                 #pragma HLS UNROLL
                 out_block.range((j+1)*32-1, j*32) =  CLIP3(((out_data[j]+add) >> shift), ap_int<32>(outputMinimum), ap_int<32>(outputMaximum));
+            }
+
+            for(int j=0; j<32; j++){
+                #pragma HLS UNROLL
+                out_block2.range((j+1)*32-1, j*32) =  CLIP3(((out_data[j+32]+add) >> shift), ap_int<32>(outputMinimum), ap_int<32>(outputMaximum));
             }
 
             //out[i] = out_block;
@@ -449,5 +461,6 @@ extern "C" void IDCT2(ap_int<512>* in, ap_int<512>* out, int block_size, int siz
             //out[i] = out_block;
         }
         out[i] = out_block;
+        out2[i] = out_block2;
     }
 }
