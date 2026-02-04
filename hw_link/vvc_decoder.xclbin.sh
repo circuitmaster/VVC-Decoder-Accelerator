c4ccea0ed5ba688b76ebad106d1159f0e37e14f3
circuitmaster <aliemre.oztas@postgrad.manchester.ac.uk>
2026-02-04 13:32:33 +0000

    idct2 64x64 optimization process

---
+++ hw_link/vvc_decoder.xclbin.sh
@@ -0,0 +1,1 @@
+/tools/Xilinx/Vitis/2022.2/bin/v++ --target hw --link --config vvc_decoder-link.cfg -ovvc_decoder.xclbin ../Hardware/build/idct2.xo ../Hardware/build/idct8.xo ../Hardware/build/idst7.xo
