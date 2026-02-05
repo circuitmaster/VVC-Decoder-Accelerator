6caffee20a2717a95a396be802b0eb7964e55ab1
Ali Emre Oztas <aliemre.oztas@manchester.ac.uk>
2026-02-05 21:37:36 +0000

    idct2 bitwise vck5000 update passed hw link

--- hw_link/vvc_decoder.xclbin.sh
+++ hw_link/vvc_decoder.xclbin.sh
@@ -1,1 +1,1 @@
-/tools/Xilinx/Vitis/2022.2/bin/v++ --target hw --link --config vvc_decoder-link.cfg -ovvc_decoder.xclbin ../Hardware/build/idct2.xo ../Hardware/build/idct8.xo ../Hardware/build/idst7.xo
+/tools/Xilinx/Vitis/2022.2/bin/v++ --target hw --link --config vvc_decoder-link.cfg -ovvc_decoder.xsa ../Hardware/build/idct2.xo
